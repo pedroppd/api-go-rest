d96a21fd0ef2d361037791f0a3cb242220650e36
pedroppd <pedro_pdantas@hotmail.com>
2022-08-07 10:49:06 -0300

    Adding gorila mux lib

--- routes/routes.go
+++ routes/routes.go
@@ -4,11 +4,13 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	controller "github.com/pedroppd/api-go-rest.git/controllers"
 )
 
 func HandleRequest() {
-	http.HandleFunc("/", controller.Home)
-	http.HandleFunc("/api/personalities", controller.GetAllPersonalities)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	r := mux.NewRouter()
+	r.HandleFunc("/", controller.Home)
+	r.HandleFunc("/api/personalities", controller.GetAllPersonalities)
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
